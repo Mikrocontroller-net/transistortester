2f9f937d03f2eb4d5c893acd1d06ec3b3ed51d5c
kubi48 <kubi48@6ebdd44f-5a7e-449c-b779-36259138d2c7>
2020-06-25 11:14:42 +0000

    Anzeige der unbenutzten Pin Nummer bei LED
    git-svn-id: svn://mikrocontroller.net/transistortester@830 6ebdd44f-5a7e-449c-b779-36259138d2c7

--- bootloaders/optiboot/show_led_pin.sh
+++ bootloaders/optiboot/show_led_pin.sh
@@ -8,34 +8,38 @@
 # You can convert the name of the target processor in MCI_TARGET to
 # AVR_FAMILY with avr_family.sh .
 
-if [ "${LED}" = "" ] ; then
- led_grep_txt="LED"
+if (( ${LED_START_FLASHES} == 0 )) && (( ${LED_DATA_FLASH} == 0)) ; then
+  echo "${Vgrau}LED-Pin not used!${Vnormal}"
 else
- led_grep_txt="^p${LED}"
-fi
-if (( ${VerboseLev} > 3 )) ; then echo "rx_zeile=grep --color=never \"${led_grep_txt}\" avr_pins/${AVR_FAMILY}.pins" ; fi
-pin_zeile=`grep --color=never "${led_grep_txt}" avr_pins/${AVR_FAMILY}.pins` 2> /dev/null
-if (( $? == 0 )) ; then
   if [ "${LED}" = "" ] ; then
-    nled=`echo ${pin_zeile} | cut -c2-3`
-    if (( ${VerboseLev} > 2 )) ; then echo "LED set to ${nrx} in list_led_pins.sh" ; fi
-    LED=${nled}
-    unset nled
-  fi
-  pin_desc=`echo "${pin_zeile}" | cut -f3`
-  pin_layout=`echo "${pin_zeile}" | cut -f2`
-  echo -n "${Vgrau}LED-Pin ${Vnormal}P${LED} ${Vgrau}use Pin ${Vnormal}${pin_layout}"
-  if [ "${pin_desc}" = "-" ] || [ "${pin_desc}" = "" ] ; then
-   echo " "
+   led_grep_txt="LED"
   else
-   echo "${Vgrau}, with special functions: ${Vnormal}${pin_desc}"
+   led_grep_txt="^p${LED}"
+  fi
+  if (( ${VerboseLev} > 3 )) ; then echo "rx_zeile=grep --color=never \"${led_grep_txt}\" avr_pins/${AVR_FAMILY}.pins" ; fi
+  pin_zeile=`grep --color=never "${led_grep_txt}" avr_pins/${AVR_FAMILY}.pins` 2> /dev/null
+  if (( $? == 0 )) ; then
+    if [ "${LED}" = "" ] ; then
+      nled=`echo ${pin_zeile} | cut -c2-3`
+      if (( ${VerboseLev} > 2 )) ; then echo "LED set to ${nrx} in list_led_pins.sh" ; fi
+      LED=${nled}
+      unset nled
+    fi
+    pin_desc=`echo "${pin_zeile}" | cut -f3`
+    pin_layout=`echo "${pin_zeile}" | cut -f2`
+    echo -n "${Vgrau}LED-Pin ${Vnormal}P${LED} ${Vgrau}use Pin ${Vnormal}${pin_layout}"
+    if [ "${pin_desc}" = "-" ] || [ "${pin_desc}" = "" ] ; then
+     echo " "
+    else
+     echo "${Vgrau}, with special functions: ${Vnormal}${pin_desc}"
+    fi
+    unset pin_layout
+    unset pin_desc
+  else
+    echo "list_led_pin.sh has not found \"${led_grep_txt}\" in avr_pins/${AVR_FAMILY}.pins"
+    echo "List of availavle pins for ${AVR_FAMILY} group:"
+    grep "^p[A-N][0-7]" avr_pins/${AVR_FAMILY}.pins
+    exit 1
   fi
   unset pin_zeile
-  unset pin_desc
-else
-  echo "list_led_pin.sh has not found \"${led_grep_txt}\" in avr_pins/${AVR_FAMILY}.pins"
-  echo "List of availavle pins for ${AVR_FAMILY} group:"
-  grep "^p[A-N][0-7]" avr_pins/${AVR_FAMILY}.pins
-  exit 1
 fi
-unset pin_layout
